f883189168fbd2357876cd4272667a39822dc1c1
Justin Landis <jtlandis@live.unc.edu>
2017-08-30 15:03:00 -0400

    Edits in the NAMESPACE file and Main.R  ... Making the default form for the drra S3 work.

--- R/main.R
+++ R/main.R
@@ -104,6 +104,7 @@ drra <- function(x, ...) UseMethod("drra")
 #' @describeIn drra Used in the default case, supplying a single dose and response variable
 #' @param dose Dose
 #' @param response Response
+#' @export
 drra.default <- function(dose, response,
                          constrained = FALSE,
                          grad = NULL,
@@ -143,7 +144,7 @@ drra.default <- function(dose, response,
 #' @description A general 4PL model fitting function for analysis of
 #'   dose-response relation.
 #'
-#' @param formula A symbolic description of the model to be fit. Either of the
+#' @param  formula A symbolic description of the model to be fit. Either of the
 #'   form 'response ~ dose' or as a data frame with response values in first
 #'   column and dose values in second column.
 #' @param data A data frame containing variables in the model.
