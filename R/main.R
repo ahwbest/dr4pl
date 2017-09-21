11c21a9c9c9797cddbef3db1ec63ba21db000186
Justin Landis <jtlandis@live.unc.edu>
2017-09-21 11:47:08 -0400

    single additional sentence discriptor of the error data sets

--- R/main.R
+++ R/main.R
@@ -425,6 +425,7 @@ NULL
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
 #' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' This data set exemplifies the case of a single extreme outlier of one dose measurement. 
 #' @title Single High Outlier
 #' @name drc_error_1
 #' @docType data
@@ -432,6 +433,7 @@ NULL
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
 #' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' This data set exemplifies the case of multiple outliers as well as a small number of observations per dose measurement.
 #' @title Multiple High Outliers at Different measurements 
 #' @name drc_error_2
 #' @docType data
@@ -439,6 +441,7 @@ NULL
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
 #' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' This data set exemplifies the case of multiple outliers at a single dose measurement as well as the support problem.
 #' @title Support Problem and Outliers at a Single Dose Level
 #' @name drc_error_3
 #' @docType data
@@ -446,6 +449,7 @@ NULL
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
 #' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' This data set exemplifies the support problem.
 #' @title Support Problem
 #' @name drc_error_4
 #' @docType data
