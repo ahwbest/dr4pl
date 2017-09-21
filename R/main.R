774252864eb5d3b59313bad6f8c734c51956c1b6
Justin Landis <jtlandis@live.unc.edu>
2017-09-21 11:38:26 -0400

    updating discriptions on sample_data_(1:13) to be accurate about which packages may use it.

--- R/main.R
+++ R/main.R
@@ -333,91 +333,91 @@ summary.dr4pl <- function(object, ...) {
 
 
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_1
 #' @name sample_data_1
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_2
 #' @name sample_data_2
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_3
 #' @name sample_data_3
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_4
 #' @name sample_data_4
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_5
 #' @name sample_data_5
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_6
 #' @name sample_data_6
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_7
 #' @name sample_data_7
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_8
 #' @name sample_data_8
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_9
 #' @name sample_data_9
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_10
 #' @name sample_data_10
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_11
 #' @name sample_data_11
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_12
 #' @name sample_data_12
 #' @docType data
 #' @keywords sample_data
 NULL
 #' These are a handful of experimentally derived datasets from the wet-laboratory.
-#' These all have numerical errors in other dose-response curve-packages, but not using these methods.
+#' These may or may not have numerical errors in other dose-response curve-packages, but definitly not using these methods.
 #' @title sample_data_13
 #' @name sample_data_13
 #' @docType data
